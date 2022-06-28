d9d953eff8709e493b907b6a5b7e7d3e31df85d4
Kevin Aguilar <kevinrock_0103@htomail.com>
2022-06-28 11:17:51 -0500

    Note for the database

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
+// Data base is not active actually is only conected to the local server 
 mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true});
 
 const userScheema = new mongoose.Schema ({
